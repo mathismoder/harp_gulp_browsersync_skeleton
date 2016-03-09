1e4c2c2d4fae9910cbe6a6215f2361e4c2d7bc2d
Mathis @ DDB <mathis_ddb@mbp-mathis.local>
2016-03-09 09:43:27 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# harp_gulp_browsersync_skeleton
-Setup up a starting point for working with HarpJS, Gulp and BrowserSync
+# Harp, Gulp And BrowserSync
+
+(derived from superhighfives/harp-gulp-browsersync-boilerplate)
+
+A ready-to-go Gulp and Harp set up that does auto css injection via BrowserSync.
+
+You can set it up via Harp with:
+
+```console
+harp init src --boilerplate superhighfives/mathismoder/harp_gulp_browsersync_skeleton
+```
+
+Modify files in /src.
+
+Gulp commands:
+- serve (serve from /src)
+- build (builds into /dist)
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var harp        = require('harp');
+var cp          = require('child_process');
+
+/**
+ * Serve the Harp Site from the src directory
+ */
+gulp.task('serve', function () {
+  harp.server(__dirname + "/src", {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: "localhost:9000",
+      open: false,
+      /* Hide the notification. It gets annoying */
+      notify: {
+        styles: ['opacity: 0', 'position: absolute']
+      }
+    });
+    /**
+     * Watch for scss changes, tell BrowserSync to refresh main.css
+     */
+    gulp.watch(["*.css", "*.sass", "*.scss", "*.less"], function () {
+      reload("main.css", {stream: true});
+    });
+    /**
+     * Watch for all other changes, reload the whole page
+     */
+    gulp.watch(["*.html", "*.ejs", "*.jade", "*.js", "*.json", "*.md"], function () {
+      reload();
+    });
+  })
+});
+
+gulp.task('build', function (done) {
+  cp.exec('harp compile src dist', {stdio: 'inherit'})
+    .on('close', done)
+});
+
+/**
+ * Default task, running `gulp` will fire up the Harp site,
+ * launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve']);
